7683088dc6686c7d379559d2ef2a96d9e4d1c6e4
Cdiazf <cdiazfarfan@gmail.com>
2023-05-07 10:54:46 -0500

    creacion de proyecto

---
+++ src/main/java/cibertec/edu/pe/dawi_cl1_diaz_farfan_cesar_augusto/DawiCl1DiazFarfanCesarAugustoApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.dawi_cl1_diaz_farfan_cesar_augusto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl1DiazFarfanCesarAugustoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawiCl1DiazFarfanCesarAugustoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cibertec/edu/pe/dawi_cl1_diaz_farfan_cesar_augusto/DawiCl1DiazFarfanCesarAugustoApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.dawi_cl1_diaz_farfan_cesar_augusto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl1DiazFarfanCesarAugustoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
